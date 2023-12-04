cad3050b047a33ff03bfd99ae58fcccb78d9be41
Brendan Bennett <bennett.brendan@yahoo.co.uk>
2023-12-04 11:25:16 +0000

    add simple atmosphere

--- src/simulator/atmos.py
+++ src/simulator/atmos.py
@@ -6,9 +6,9 @@ def simple_atmos(state, time, earth_radius, surf_density):
     Args:
         state (tuple[float]): state vector (x1, x2, [x3], v1, v2, [v3])
         time (float): time of state
-        
-        kwargs:
-            earth_radius: radius of earth's surface
-            surf_density: density of earth's surface
+        earth_radius (float): radius of earth's surface
+        surf_density (float): density of earth's surface
     """
-    return None
+    dim = len(state)
+    
+    return surf_density * np.exp(-(np.norm(state[:dim])/earth_radius))
