9ea44efafbbea4264407b478f069079ae18968f1
Brendan <bennett.brendan@yahoo.co.uk>
2024-05-09 00:36:16 +0100

    Simpler saving now just uses a save name.

--- tests/test_data_io.py
+++ tests/test_data_io.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+from pathlib import Path
 
 from deorbit.simulator import Simulator, generate_sim_config
 from deorbit.utils.constants import EARTH_RADIUS
@@ -22,10 +23,20 @@ def test_save_simdata(tmpdir, format):
     sim.run(10)
     pre_save_data = sim.gather_data()
     pre_save_config = sim.export_config()
-    save_path = sim.save_data(tmpdir, format)
+    
+    test_save_name = "test_dir"
+    save_path = sim.save_data(tmpdir / Path(test_save_name), format)
+    
+    assert save_path == tmpdir / Path(test_save_name)
 
     loaded_data = load_sim_data(save_path)
     loaded_config = load_sim_config(save_path)
 
     assert pre_save_data == loaded_data
     assert pre_save_config == loaded_config
+    
+    loaded_data_2 = load_sim_data(tmpdir / Path(test_save_name))
+    loaded_config_2 = load_sim_config(tmpdir / Path(test_save_name))
+    
+    assert pre_save_data == loaded_data_2
+    assert pre_save_config == loaded_config_2
