52b8394b05a6f5a8d4e58e3ff173a961c6813c32
jmedluca <jimmy.medici77@gmail.com>
2024-05-10 10:15:36 +0100

    Noise and Documentation

--- src/deorbit/observer/observer.py
+++ src/deorbit/observer/observer.py
@@ -7,45 +7,47 @@ from deorbit.utils.constants import EARTH_RADIUS, EARTH_ROTATIONAL_SPEED
 
 
 class Observer:
-    """
-    HOW TO (current, subject to change hopefully):
-    -self.states and self.times must be initially set to the states and times from the simulator.
-    -self.number_of_radars can be specified (by default is 1) and by default they are give equally spaced positions (longitude-latitude)
-    around the equator. self.positions_of_radars can be set in list[long, lat] format for custom positions.
-
-    METHODS:
-    -self.plot_config() method shows a plot of the radar station positions
-    -self.run(checking_interval) runs the radar simulation but self.states and self.times needs setting first.
-
-
-    THINGS TO DO STILL:
-    - Test if this even works...
-    - Make it more user friendly and just layed out better
-    - self.run() method should generate an instance of ObsData which will be used going forward in the Predictor.
-    - add explicit type checking
-    - noise covariance to radar observations
-    """
-
     def __init__(self, **kwargs):
+        """
+        ATTRIBUTES:
+        :::Earth Model Configuration:::
+        self.radius: Sets the distance of the Earth's radius
+        self.rotation: Sets the speed in rad/s of the Earth's rotation
+
+        :::Radar Stations Configuration:::
+        self.number_of_radars: Determines how many radar stations are used in the simulation, default 1
+        self.positions_of_radars: Optional numpy array containing [longitude, latitude] for each satellite, otherwise default positions set
+        self.radar_variance_per_m: Simulates the uncertainty in the measurement. The variance increases linearly as distance to satellite increases. Default 0.1
+
+        :::Observer Output:::
+        self.observed_states: After the Observer has been run, observed states are stored here to be used in the Predictor
+        self.observed_times: After the Observer has been run, observed times are stored here to be used in the Predictor
+
+        METHODS:
+        self.plot_config(): Shows a 3D plot of the radar station configuration
+
+        self.run(sim_times, sim_states, checking_interval):
+        ->sim_states: the states of the satellite given by the Simulator
+        ->sim_times: the time steps of each state given by the Simulator
+        ->checking_interval: the size of the jump between times in sim_times in which the radar stations are checking for line of sight
+        """
         self.radius: float = kwargs.get("radius", EARTH_RADIUS)
         self.rotation: float = kwargs.get("rotation", EARTH_ROTATIONAL_SPEED)
         self.number_of_radars: int = kwargs.get("number_of_radars", 1)
         self.positions_of_radars: npt.NDArray = kwargs.get(
             "positions_of_radars", self._default_radar_positions(self.number_of_radars)
         )
-        self.states: npt.NDArray | None = kwargs.get("states", None)
-        self.times: npt.NDArray | None = kwargs.get("times", None)
+        self.radar_variance_per_m: float = kwargs.get("radar_noise_factor", 0.1)
         self.observed_states: list[list[float]] | None = None
         self.observed_times: list[float] | None = None
-        self.radar_noise_factor: float = kwargs.get("radar_noise_factor", 1) 
+
 
         self._radar_position_validator()
 
     @classmethod
     def _default_radar_positions(cls, number_of_radars: int) -> npt.NDArray:
         """
-        Validator to check whether 'positions_of_radars' is in the correct format,
-        if it is None then default positions are given as equally spaced radars around the equator
+        Sets default radar positions, returns longitude and latitude of radars equally spaced around the equator.
         """
         delta_lon = (np.pi * 2) / number_of_radars
         rad_default_positions = np.zeros(shape=(number_of_radars, 2))
@@ -55,14 +57,18 @@ class Observer:
         return rad_default_positions
 
     def _radar_position_validator(self):
-        # Convert list to np.array if it's not None and validate shape
+        """
+        Validates shape of the numpy array containing radar positions
+        """
         if self.positions_of_radars.shape != (self.number_of_radars, 2):
             raise ValueError(
                 f"positions_of_radars must be a numpy array with shape ({self.number_of_radars}, 2)"
             )
 
     def _rad_xyz(self, longlat):
-        """Gives x and y on the Earth surface at a specified longitude and latitude"""
+        """
+        Gives x and y on the Earth surface at a specified longitude and latitude
+        """
         x_earth = self.radius * np.cos(longlat[1]) * np.sin(longlat[0])
         z_earth = self.radius * np.sin(longlat[1]) * np.sin(longlat[0])
         y_earth = self.radius * np.cos(longlat[0])
@@ -71,16 +77,25 @@ class Observer:
     
     def _measurement_noise(self, rad_longlat, sat_state):
         """
-        Method which returns a covariance matrix for the observed state. A linear function increasing the noise as
-        distance between the radar and the satellite increases
+        Method which returns the observed state with additional measurement noise sampled from a multivariate Gaussian. 
+        The noise increases linearly as distance between the radar and the satellite increases.
         """
         distance = np.linalg.norm(sat_state[0:3] - self._rad_xyz(rad_longlat))
-        variance = distance * self.radar_noise_factor
+        variance = distance * self.radar_variance_per_m
 
         cov = np.diag([variance, variance, variance, variance])
         noisy_state = np.random.multivariate_normal(sat_state, cov)
 
         return noisy_state
+    
+    def _check_los(self, longlat, state):  
+        """
+        Checking line of sight using radar longlat and satellite state
+        """
+        if np.dot(self._rad_xyz(longlat), (state[0:3]-self._rad_xyz(longlat))) >= 0:
+            return True
+        else:
+            return False
 
     def plot_config(self):
         """
@@ -119,19 +134,20 @@ class Observer:
         ax.set_title("Earth Radar Station Config")
         plt.show()
 
-    def run(self, checking_interval):
+    def run(self, sim_times, sim_states, checking_interval):
         """
         Runs the simulation of the radar measurements taken at regular intervals specified by 'checking_interval'.
         Returns self.observed_times and self.observed_states containing the times and states from the simulation
         where the satellite has been observed by a radar station.
         """
+
         times_observed = []
         states_observed = []
 
         # Only checking the states at a regular interval specified by time_step
         radar_initial_positions = self.positions_of_radars
-        times_checked = self.times[::checking_interval]
-        states_checked = self.states[::checking_interval]
+        times_checked = sim_times[::checking_interval]
+        states_checked = sim_states[::checking_interval]
 
         for i, xi in enumerate(times_checked):
             for radar, longlat in enumerate(radar_initial_positions):  # For each radar:
@@ -150,24 +166,3 @@ class Observer:
 
         self.observed_times = np.array(times_observed)
         self.observed_states = np.array(states_observed)
-
-    def _check_los(
-        self, longlat, state
-    ):  # Checking line of sight using radar longlat and satellite state
-        if np.dot(self._rad_xyz(longlat), (state[0:3]-self._rad_xyz(longlat))) >= 0:
-            return True
-        else:
-            return False
-
-
-'''
-    def _earth_rad(self, latitude): 
-        """Gives radius of earth at a specified latitude (rad) (longitude does not affect radius) for oblate spheroid"""
-        if self.config.max_radius == self.config.min_radius: #if the earth is modelled as a sphere
-            return EARTH_RADIUS
-        else: #else the earth is an oblate spheroid
-            a = self.config.max_radius
-            b = self.config.min_radius
-            radius = np.sqrt(((a**2 * np.cos(latitude))**2 + (b**2 * np.sin(latitude))**2) / ((a * np.cos(latitude))**2 + (b * np.sin(latitude))**2))
-            return radius
-'''
